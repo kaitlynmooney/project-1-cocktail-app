a4080611dcc41d24ffa4dac0ae8fe57c5f00576d
Siena Schipke <sienaschipke@gmail.com>
2024-06-13 11:05:49 -0700

    added dynamic functionality for cocktail card

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const modalEl = $(".modal");
 const searchBtn = $("#searchButton");
 const cancelBtn = $("#cancelButton");
 const generateBtn = $('#generateBtn');
+const featuredCocktailCard = $('#featured-cocktail');
 
 // DATA
 
@@ -23,8 +24,8 @@ const generateBtn = $('#generateBtn');
 const openModal = () => {
     modalEl.addClass('is-active')
 }
-// send inputted ingredient to the API call 
-
+// // send inputted ingredient to the API call 
+// 
 
 // removes .is-active from modal element and closes it
 const closeModal = () => {
@@ -44,7 +45,7 @@ const getCocktails = function(ingredient) {
             console.log(result);
             // save recipes to local storage 
             localStorage.setItem('recipes', JSON.stringify(result));
-
+            displayFeaturedCocktail(result);
         },
         error: function ajaxError(jqXHR) {
             console.error('Error: ', jqXHR.responseText);
@@ -66,9 +67,58 @@ const getCocktailNames = function() {
 
 
 // –––Display the first five cocktails in the Featured Cocktail Section–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
-const displayFeaturedCocktail = function() {
-}
+const displayFeaturedCocktail = function(result) {
+    featuredCocktailCard.append(`
+          <h3 class="is-size-1 card-header-title is-centered">${toTitleCase(result[0].name)}</h3>
+          <div class="is-flex is-justify-content-center" s>
+            <div class="card-image">
+                <img src="https://www.liquor.com/thmb/YbaFLqBKww1EdvE4ojPNe5sFjzg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/dirty-martini-1500x1500-hero-6cbd60561031409ea1dbf1657d05cb2d.jpg" alt="dirty martini" style="height:250px;width:250px;border-radius: 5px" class="p-4"/>
+            </div>
+              <div class="is-flex is-align-items-center is-align-self-center card-content" >
+                  <div class="is-flex-direction-column mx-6 is-align-self-flex-start" id="ingredientContainer">
+                      <h4 class="is-size-2 pb-3">Ingredients</h4>
+                      <ul style="list-style: inside; list-style-type: circle">
+                      ${listIngredients(result[0].ingredients)}
+                      </ul>
+                  </div>
+                  <div class="is-flex-direction-column ml-5" id="recipeContainer">
+                    <h4 class="is-size-2 pb-3">Recipe</h4>
+                    <ol style="list-style: inside; list-style-type: decimal">
+                        ${listRecipe(result[0].instructions)}
+                    </ol>
+                  </div>
+              </div>
+          </div>
+          <subsection class="is-flex is-justify-content-space-evenly is-align-items-center">
+              <button id="generateBtn" class="button is-normal is-responsive is-size-4" style="background-color: var(--primary); color: var(--light-text)">Generate Another Cocktail</button>
+              <button id="generateBtn" class="button is-normal is-responsive is-size-4" style="background-color: var(--primary); color: var(--light-text)">Save to Cocktail Library</button>
+          </subsection>
+    `);
+};
 
+const toTitleCase = (nameString) => {
+    return nameString.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
+
+const listIngredients = (ingredientArray) => {
+    let ingredientList = '';
+    for (const ingredient of ingredientArray){
+        ingredientList += `<li>${ingredient}</li>`;
+    };
+    return ingredientList;
+};
+
+const listRecipe = (recipeString) => {
+    const recipeArray = recipeString.split('.');
+    recipeArray.pop();
+    let recipe = '';
+    for (const item of recipeArray){
+        recipe += `<li>${item}</li>`
+    }
+    return recipe;
+};
 //––API Call to get photos of the 5 cocktails –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 const cocktailPhoto = function() {
     
@@ -83,3 +133,4 @@ generateBtn.on('click', getCocktails);
 
 // INTIALIZATIONS
 getCocktails(ingredient); 
+
