c6c9a3bee074c2975ab4ba4bf2256aa7d36ffc93
Arcaneghost24 <jmpresto1@yahoo.com>
2024-06-13 11:15:14 -0400

    update carousel code

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,34 +72,34 @@ cancelBtn.on('click', closeModal);
 //     });
 //   });
 
-// use the inputted ingredient to make an API call and get 
-const getCocktails = function(ingredient) {
+// // use the inputted ingredient to make an API call and get 
+// const getCocktails = function(ingredient) {
 
-  $.ajax({
-      method: 'GET',
-      url: 'https://api.api-ninjas.com/v1/cocktail?ingredients=' + ingredient,
-      headers: { 'X-Api-Key': '+frO7azzghiOsVVmW/5Bjg==OOWySxn7ztVE6WsV'},
-      contentType: 'application/json',
-      success: function(result) {
-          console.log(result);
-      },
-      error: function ajaxError(jqXHR) {
-          console.error('Error: ', jqXHR.responseText);
-      }
-  });
-}
-function readIngredientsFromStorage(){
-let ingredients = JSON.parse(localStorage.getItem('ingredients'));
-if (!ingredients){
-  ingredients=[];
-}
-return ingredients;
-}
-function saveIngredientsToStorage(ingredients) {
-  localStorage.setItem('ingredients', JSON.stringify(ingredients));
-}
-readIngredientsFromStorage();
-saveIngredientsToStorage();
+//   $.ajax({
+//       method: 'GET',
+//       url: 'https://api.api-ninjas.com/v1/cocktail?ingredients=' + ingredient,
+//       headers: { 'X-Api-Key': '+frO7azzghiOsVVmW/5Bjg==OOWySxn7ztVE6WsV'},
+//       contentType: 'application/json',
+//       success: function(result) {
+//           console.log(result);
+//       },
+//       error: function ajaxError(jqXHR) {
+//           console.error('Error: ', jqXHR.responseText);
+//       }
+//   });
+// }
+// function readIngredientsFromStorage(){
+// let ingredients = JSON.parse(localStorage.getItem('ingredients'));
+// if (!ingredients){
+//   ingredients=[];
+// }
+// return ingredients;
+// }
+// function saveIngredientsToStorage(ingredients) {
+//   localStorage.setItem('ingredients', JSON.stringify(ingredients));
+// }
+// readIngredientsFromStorage();
+// saveIngredientsToStorage();
 
 // Carousel functionality in separate block for now, can refactor .js later to organize everything
 // Implement Jquery later
@@ -108,37 +108,42 @@ let cocktailIndex = 0; //will later be pulled from local storage
 const carousel = document.querySelector('.cocktail-library');
 const nextBtn = carousel.querySelector('.next');
 const prevBtn = carousel.querySelector('.prev');
+let currentCocktail = document.querySelector('#carouselCurrent');
+
 
 // Data
 let myCocktails = [
   'assets/images/bloody-mary.jpg',
   'assets/images/mojito.jpg',
   'assets/images/white-russian.jpg'
-]
+];
+
+
+console.log(cocktailIndex)
 //Functions
 
-function showCocktail(n) {
-  $('.cocktail-library').append("<img>", `${myCocktails[n]}`);
+function showCocktail(cocktailIndex) {
+  carousel.style.backgroundImage = `url(${myCocktails[cocktailIndex]})`;
+  console.log(currentCocktail);
 }
 
-function nextCocktail(n) {
-  cocktailIndex += n;
-  if (n > myCocktails.length) {
+function nextCocktail() {
+  cocktailIndex++ ;
+  if (cocktailIndex > myCocktails.length) {
     cocktailIndex = 0};
-}
-function prevCocktail(n) {
-  cocktailIndex -= n;
-  if (n < 0) {
-    cocktailIndex = myCocktails.length}
-}
-
-function showCocktail(n) {
-
-  for (let i = 0; i < myCocktails.length; i++) {
-    myCocktails[i].style.display = "none";  
+  console.log(cocktailIndex);
+  showCocktail()
   }
+  function prevCocktail() {
+    cocktailIndex-- ;
+    if (cocktailIndex < 0) {
+      cocktailIndex = myCocktails.length};
+      console.log(cocktailIndex);
+    showCocktail()
+}
 
-  myCocktails[cocktailIndex].style.display = "block";  
+function showCocktail(cocktailIndex) {
+  carousel.style.backgroundImage = myCocktails[cocktailIndex]
 
 }
 
@@ -147,4 +152,4 @@ nextBtn.addEventListener("click", nextCocktail);
 prevBtn.addEventListener("click", prevCocktail);
 
 // Initialization
-showCocktail(cocktailIndex);
+showCocktail();
